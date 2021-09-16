27163858392de8a92df4fa0709377139005a2d88
dwaynediddy <burrows.dwayne@icloud.com>
2021-09-16 10:55:59 +0200

    added basic product sliders

--- src/App.js
+++ src/App.js
@@ -2,30 +2,34 @@ import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
-// import TopNav from './components/navigation/TopNav'
+import TopNav from './components/navigation/TopNav'
 // import SideBar from './components/navigation/SideBar'
-import SideMenu from './components/navigation/SideMenu'
+// import SideMenu from './components/navigation/SideMenu'
 import Landing from './components/Landing'
 import Banner from './components/Banner'
 import MainItem from './components/faders/MainItem'
+import SecondaryItem from './components/faders/SecondaryItem'
+import ThirdItem from './components/faders/ThirdItem'
 import ContactUs from './components/ContactForm'
 
 function App() {
   return (
     <>
       <Router className="App">
-          {/* <TopNav /> */}
+          <TopNav />
           {/* <SideBar /> */}
-          <SideMenu /> 
+          {/* <SideMenu />  */}
         <Switch>
 
           <Route path="/" />
         </Switch>
         <Landing />
         <Banner />
-        <div className="products">
         <MainItem />
-        </div>
+        <SecondaryItem />
+        <ThirdItem />
+        {/* <div className="products">
+        </div> */}
         <ContactUs />
       </Router>
     </>
--- src/components/faders/ThirdItem.js
+++ src/components/faders/ThirdItem.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import "animate.css"
 
+import "../../style/mainitem.css"
+
+
+import mainProduct from '../../images/logo2.png'
+
 const ThirdItem = () => {
     return (
-        <div>
-            
+        <div className="third-container">
+            <div className="animate__animated animate__fadeInLeft">
+                <h1>An animated element</h1>
+                <p>An animated element</p>
+            </div>
+            <div className="animate__animated animate__fadeInRight">
+                <img src={mainProduct} alt='product name' />
+            </div>
         </div>
     )
 }
