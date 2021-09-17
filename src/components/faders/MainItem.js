79c2e29d61cc3bb318bd001c56ed7bcfd3d2b79a
dwaynediddy <burrows.dwayne@icloud.com>
2021-09-17 15:32:28 +0200

    animate on scroll added to page basics working

--- src/components/faders/MainItem.js
+++ src/components/faders/MainItem.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import "animate.css"
+import React, { useEffect } from 'react'
 
 import mainProduct from '../../images/logo2.png'
 
 import "../../style/mainitem.css"
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
 
 const productName = 'An animated element'
 const productDescription = "Product Description lorum epsum"
 
 
 const MainItem = () => {
+    useEffect(() => {
+        AOS.init({duration: 2000})
+    }, [])
+
     return (
         <div className="main-container">
-            <div className="animate__animated animate__fadeInLeft">
+            <div class="grids">
                 <h1>{productName}</h1>
-                <br />
                 <p>{productDescription}</p>
+                    <div className='products' data-aos="fade-up">1</div>
+                    <div className='products' data-aos="fade-up">2</div>
+                    <div className='products' data-aos="fade-up">3</div>
             </div>
-            <div className="animate__animated animate__fadeInRight">
+            <div className="wow fadeInRight">
                 <img src={mainProduct} alt='product name' />
             </div>
         </div>
