c877ccab8e36e693794ab1db8a55d549cb076e9e
dwaynediddy <burrows.dwayne@icloud.com>
2021-09-17 17:11:46 +0200

    faders now fading in to basic position

--- src/components/faders/MainItem.js
+++ src/components/faders/MainItem.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 
+// import { ProductData } from '../products/Productdata'
+
 import mainProduct from '../../images/logo2.png'
 
 import "../../style/mainitem.css"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-const productName = 'An animated element'
+const productName = 'Product'
 const productDescription = "Product Description lorum epsum"
 
 
@@ -18,16 +20,35 @@ const MainItem = () => {
     return (
         <div className="main-container">
             <div class="grids">
-                <h1>{productName}</h1>
-                <p>{productDescription}</p>
-                    <div className='products' data-aos="fade-right">
-                        <img src={mainProduct} alt='product name' />
+                    <div className="products">
+                        <div className='products-data' data-aos="fade-right">
+                            <h1 className='productName'>{productName}</h1>
+                            <p className='productDescription'>{productDescription}</p>
+                            <button>shop</button>
+                        </div>
+                        <div className='products-image' data-aos="fade-left">
+                            <img src={mainProduct} alt='product name' />
+                        </div>
                     </div>
-                    <div className='products' data-aos="fade-left">
-                        <img src={mainProduct} alt='product name' />
+                    <div className="products">
+                        <div className='products-data' data-aos="fade-right">
+                            <img src={mainProduct} alt='product name' />
+                        </div>
+                        <div className='products-image' data-aos="fade-left">
+                            <h1 className='productName'>{productName}</h1>
+                            <p className='productDescription'>{productDescription}</p>
+                            <button>shop</button>
+                        </div>
                     </div>
-                    <div className='products' data-aos="fade-right">
-                        <img src={mainProduct} alt='product name' />
+                    <div className="products">
+                        <div className='products-data' data-aos="fade-right">
+                            <h1 className='productName'>{productName}</h1>
+                            <p className='productDescription'>{productDescription}</p>
+                            <button>shop</button>
+                        </div>
+                        <div className='products-image' data-aos="fade-left">
+                            <img src={mainProduct} alt='product name' />
+                        </div>
                     </div>
             </div>
             <div className="wow fadeInRight">
---
+++ src/components/products/Productdata.js
@@ -0,0 +1,19 @@
+export const ProductData = [
+    {
+        productName: 'Product one',
+        productDescription: 'Product description',
+        productImage: 'Product image'
+    },
+
+    {
+        productName: 'Product two',
+        productDescription: 'Product description',
+        productImage: 'Product image'
+    },
+
+    {
+        productName: 'Product three',
+        productDescription: 'Product description',
+        productImage: 'Product image'
+    },
+]
