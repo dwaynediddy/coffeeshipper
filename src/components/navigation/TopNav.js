44c6193db6e9a25ba04219def66441f3cd840455
dwaynediddy <burrows.dwayne@icloud.com>
2021-09-20 12:19:33 +0200

    media screen and button created

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Link } from 'react-router-dom'
 
 import '../../style/nav.css'
@@ -8,21 +8,28 @@ import { SideBarData } from './SideBarData'
 import logo from '../../images/logo.png'
 
 const TopNav = () => {
+    const [ showLinks, setShowLinks ] = useState(false)
+
     return (
         <div className="nav">
             <img src={logo} className="logo" alt="logo" />
             <ul className='menu-item'>
                     {SideBarData.map((item, index) => {
                             return (
+                                <>
                                 <li key={index} className={item.cName}>
                                     <Link to={item.path}>
                                         {item.icon}
                                         <span>{item.title}</span>
                                     </Link>
                                 </li>
+                                </>
                             )
                         })}
                 </ul>
+                <button>
+                    open
+                </button>
         </div>
     )
 }
