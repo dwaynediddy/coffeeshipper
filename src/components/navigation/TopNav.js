7409c79306c9cb482214e8ddf799832d17062925
dwaynediddy <burrows.dwayne@icloud.com>
2021-09-20 13:05:15 +0200

    dropdown menu in place working on state

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -8,10 +8,10 @@ import { SideBarData } from './SideBarData'
 import logo from '../../images/logo.png'
 
 const TopNav = () => {
-    const [ showLinks, setShowLinks ] = useState(false)
+    const [ inactive, setInactive ] = useState(false)
 
     return (
-        <div className="nav">
+        <div className={`nav ${inactive ? 'active' : ''}`}>
             <img src={logo} className="logo" alt="logo" />
             <ul className='menu-item'>
                     {SideBarData.map((item, index) => {
@@ -27,9 +27,22 @@ const TopNav = () => {
                             )
                         })}
                 </ul>
-                <button>
-                    open
+                <button onclick={() => setInactive(!inactive)} className="toggle-dropdown">
                 </button>
+                    <ul className={`drop-nav ${inactive ? 'active' : ''}`}>
+                        {SideBarData.map((item, index) => {
+                                return (
+                                    <>
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                    </>
+                                )
+                            })}
+                    </ul>
         </div>
     )
 }
